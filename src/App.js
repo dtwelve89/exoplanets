b33a4eb6b152ab1bd05e5cfd6d3df0d75436cfcf
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-11-17 14:26:52 -0800

    Added test data and loaded properly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Dropdowns from './components/Dropdowns/Dropdowns';
 import Scatterplot from './components/Scatterplot/Scatterplot';
 import * as d3 from 'd3';
-import phl from './data/phl_hec_all_confirmed.csv';
+// import phl from './data/phl_hec_all_confirmed.csv';
+import test from './data/test.csv';
 import './App.css';
 
+let loaded = false;
+
 const App = () => {
-  // const [data, setData] = useState();
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    d3.csv(phl, result => {
-      // setData(result);
-      console.log(result);
-    });
+    // let arr = [];
+    // d3.csv(test, result => {
+    //   arr.push(result);
+    // });
+    // console.log(arr);
+    d3.csv(test).then(setData);
   }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      console.log(data);
+    } else {
+      loaded = true;
+    }
+  });
 
   return (
     <div className='container'>
