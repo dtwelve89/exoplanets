2c3423bbb0c43ee9c4a33cffe59a4b8363bff56c
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-11-16 16:17:47 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Exoplanets</h1>
+    </div>
+  );
+};
+
+export default App;
