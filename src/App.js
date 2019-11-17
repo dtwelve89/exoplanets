43044f1141451e67ef1e0bf5dd8620eea4705eef
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-11-17 11:42:59 -0800

    Added data and loaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+import phl from './phl_hec_all_confirmed.csv';
 import './App.css';
 
 const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    d3.csv(phl, result => {
+      // setData(result);
+      console.log(result);
+    });
+  }, []);
+
   return (
     <div className='App'>
       <h1>Exoplanets</h1>
+      {/* <div>{data}</div> */}
     </div>
   );
 };
